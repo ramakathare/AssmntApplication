820622aae2d82e6a4e6815a4964c22e3151d64d8
Rama Kathare <ramakathare@gmail.com>
2015-10-20 15:57:42 +0530

    Tested Mongo Crud

--- Assmnt.WebApi/Controllers/ValuesController.cs
+++ Assmnt.WebApi/Controllers/ValuesController.cs
@@ -1,40 +1,53 @@
-﻿using System;
+﻿using Assmnt.WebApi.Properties;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Assmnt.WebApi.Controllers
 {
-    [Authorize]
-    public class ValuesController : ApiController
+    //[Authorize]
+    public class EmployeesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        // GET api/Employees
+        public IEnumerable<Employee> Get()
         {
-            return new string[] { "value1", "value2" };
+            var client = new MongoClient(Settings.Default.MongoConnection);
+            var db = client.GetDatabase(Settings.Default.DBName);
+            return db.GetCollection<Employee>("Employees").AsQueryable();
         }
 
-        // GET api/values/5
+        // GET api/Employees/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
-        public void Post([FromBody]string value)
+        // POST api/Employees
+        public async void Post(Employee emp)
         {
+            var client = new MongoClient(Settings.Default.MongoConnection);
+            var db = client.GetDatabase(Settings.Default.DBName);
+            var collection = db.GetCollection<Employee>("Employees");
+            collection.InsertOneAsync(emp);
         }
 
-        // PUT api/values/5
+        // PUT api/Employees/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        // DELETE api/Employees/5
+        public void Delete(Employee emp)
         {
+            var client = new MongoClient(Settings.Default.MongoConnection);
+            var db = client.GetDatabase(Settings.Default.DBName);
+            var collection = db.GetCollection<Employee>("Employees");
+            collection.DeleteOneAsync(p=>p.name.Equals(emp.name));
         }
     }
 }
---
+++ Assmnt.WebApi/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assmnt.WebApi.Controllers
+{
+    public class Employee
+    {
+        [BsonId]
+        public object _id{get;set;}
+        public string name {get;set;}
+    }
+}
